7a474df7a5ba6688443bfbc4457e0a846b27df06
Ah3ron <ah3ron@protonmail.com>
2024-11-19 01:31:19 +0300

    Remove commented-out default options in Svelte adapter configuration

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -5,8 +5,6 @@ export default {
 	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html',
