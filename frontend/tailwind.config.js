1728170bfa65fedddd46bb832ba6b355f9276c5d
Ah3ron <ah3ron@protonmail.com>
2024-11-23 18:40:52 +0300

    Update Tailwind theme and remove default body background color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,15 +11,16 @@ export default {
 	plugins: [daisyui],
 	daisyui: {
 		themes: [
-			{
-				myTheme: {
-					primary: '#dea19f',
-					secondary: '#8a2521',
-					accent: '#ff80b7',
-					neutral: '#262626',
-					'base-100': '#171717'
-				}
-			}
+			// {
+			// myTheme: {
+			// 		primary: '#dea19f',
+			// 		secondary: '#8a2521',
+			// 		accent: '#ff80b7',
+			// 		neutral: '#262626',
+			// 		'base-100': '#171717'
+			// 	}
+			// }
+			'emerald'
 		]
 	}
 };
